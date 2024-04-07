fff95c4b505a233fc85394089b5ef9768efe697c
Caleb <mensahkhalib28@gmail.com>
2024-04-07 08:17:11 +0000

    creating pages and components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,23 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from './pages/Home'
+import SignUp from "./pages/SignUp"
+import SignIn from "./pages/SignIn"
+import Dashboard from "./pages/Dashboard"
+import About from "./pages/About"
+import Projects from "./pages/Projects"
 
 const App = () => {
   return (
-    <div className="text-red-500">App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="sign-up" element={<SignUp />} />
+        <Route path="sign-in" element={<SignIn />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="about" element={<About />} />
+        <Route path="projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
