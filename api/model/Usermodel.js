093cc582c6672c184f46e4bdb35ddca6e71f1d85
Caleb <mensahkhalib28@gmail.com>
2024-04-07 23:51:41 +0000

    create user model

---
+++ api/model/Usermodel.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose'
+
+// sets the rules for what will be required to sign in
+const UserSchema = new mongoose.Schema({
+    username:{
+        type: String, 
+        required: true, 
+        unique: true
+    },
+    email:{
+        type: String, 
+        required: true, 
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+},
+{timestamps: true}
+)
+
+// create model user
+const User = mongoose.model('User',UserSchema)
+
+export default User
